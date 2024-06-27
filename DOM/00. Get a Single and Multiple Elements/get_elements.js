5fd5d653d1fb346fafd4a1397c84dbe0ca55a84b
abaltazapinto <andrebaltazarpinto@gmail.com>
2024-06-27 19:42:51 +0100

    goggo

---
+++ DOM/00. Get a Single and Multiple Elements/get_elements.js
@@ -0,0 +1,9 @@
+// const el = document.getElementById('home')
+// console.log(el)
+
+const link = document.querySelectorAl('a');
+console.log(link);
+
+// links.forEach(link => {
+//   console.log(link);  
+// })
---
+++ DOM/00. Get a Single and Multiple Elements/index.js
@@ -0,0 +1,20 @@
+// const el = document.getElementById('home')
+// console.log(el)
+
+// const links = document.querySelectorAll('a');
+// // console.log(link);
+
+// links.forEach(link => {
+//   console.log(link);  
+// })
+
+
+// links.forEach(link => {
+//   if (link.matches('[href="/login"]')) {
+//     console.log(link);
+//   }
+// })
+
+
+const divs = document.getElementsByTagName('div')
+console.log(divs);

f21eaf35062d02e4d5f57ee3bb252a92f12b1d4f
abaltazapinto <andrebaltazarpinto@gmail.com>
2024-06-30 17:35:52 +0100

    done

--- Async JavaSript/04. Challenge: Fetch API/challenge.js
+++ Async JavaSript/04. Challenge: Fetch API/challenge.js
@@ -22,3 +22,16 @@ fetch('https://jsonplaceholder.typicode.com/users/3')
     .then(response => response.json())
     .then(data => console.log(data))
     .catch(error => console.log(error));
+
+// Solution:
+fetch("https://jsonplaceholder.typicode.com/users/3")
+.then(response => {
+    if (!response.ok) {
+        throw new Error(response.status);
+    }
+    return response.json();
+})
+.then(person => {
+    console.log(`${person.name} works for ${person.company.name}`);
+})
+.catch(err => console.log(err));
